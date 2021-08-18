9bf6527a309b393e2619fbfce1f703b2f5361c07
Hacerx <pepesoler98@gmail.com>
2021-08-18 14:00:14 +0200

    Add files via upload

---
+++ Unir Pdfs/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Unir_Pdfs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seleccionar Archivos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(150, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(475, 262);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(132, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pepe Soler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(634, 283);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Unir PDFs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Unir Pdfs/Form1.cs
@@ -0,0 +1,92 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unir_Pdfs
+{
+    public partial class Form1 : Form
+    {
+
+        string[] pdfsIniciales = new string[0];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateMergedPDF()
+        {
+            SaveFileDialog rutaFinal = new SaveFileDialog();
+            rutaFinal.Filter = "PDF (*.pdf)|*.pdf";
+            if (rutaFinal.ShowDialog() == DialogResult.OK) {
+                String targetPDF = rutaFinal.FileName;
+                using (FileStream stream = new FileStream(targetPDF, FileMode.Create))
+                {
+                    Document pdfDoc = new Document(PageSize.A4);
+                    PdfCopy pdf = new PdfCopy(pdfDoc, stream);
+                    pdfDoc.Open();
+                    int division = 100 / (pdfsIniciales.Length);
+                    //var files = Directory.GetFiles(sourceDir);
+                    foreach (string file in pdfsIniciales)
+                    {
+                        if (file.Split('.').Last().ToUpper() == "PDF")
+                        {
+                            pdf.AddDocument(new PdfReader(file));
+                            progressBar1.Value += division;
+                        }
+                    }
+                    progressBar1.Value = 100;
+                   
+                    if (pdfDoc != null)
+                        pdfDoc.Close();
+                    richTextBox1.Text = "";
+                    Array.Resize(ref pdfsIniciales, 0);
+                    richTextBox1.Text = "¡Archivo generado correctamente!";
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 100) {
+                progressBar1.Value = 0;
+                richTextBox1.Text = "";
+            }
+            OpenFileDialog archivos = new OpenFileDialog();
+            archivos.Filter = "PDF (*.pdf)|*.pdf";
+            archivos.Multiselect = true;
+            if (archivos.ShowDialog() == DialogResult.OK) {
+                foreach (String archivo in archivos.FileNames) {
+                    Array.Resize(ref pdfsIniciales, pdfsIniciales.Length + 1);
+                    pdfsIniciales[pdfsIniciales.Length - 1] = archivo;
+                    richTextBox1.Text += archivo + "\n";
+                }
+                //textBox1.Text = archivos.FileName;
+
+            }
+            /*foreach (String aux in pdfsIniciales) {
+                textBox1.Text += aux;
+            }*/
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateMergedPDF();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
